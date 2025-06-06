e5b09d7337956d43a946b52d28fe456e649797d3
Jean Delvare <jdelvare@suse.de>
2025-06-06 16:03:49 +0200

    Rename variable "div"
    On some systems, "div" is a global symbol, so for portability it's
    better to avoid using that name for a local variable.
    
    Signed-off-by: Jean Delvare <jdelvare@suse.de>

--- dmidecode.c
+++ dmidecode.c
@@ -326,7 +326,7 @@ void dmi_print_memory_size(const char *attr, u64 code, int shift)
 /* shift is 0 if the value is in bytes, 1 if it is in kB, 2 if it is in MB */
 void dmi_print_storage_size(const char *attr, u64 code, unsigned int shift)
 {
-	u64 div;
+	u64 divisor;
 	static const char *unit[8] = {
 		"bytes", "kB", "MB", "GB", "TB", "PB", "EB", "ZB"
 	};
@@ -335,14 +335,14 @@ void dmi_print_storage_size(const char *attr, u64 code, unsigned int shift)
 	 * We want to choose the unit which will let us display a number
 	 * between 1.0 and 999.9.
 	 */
-	div = 1;
-	while (code / div >= 1000 && shift + 1 < ARRAY_SIZE(unit))
+	divisor = 1;
+	while (code / divisor >= 1000 && shift + 1 < ARRAY_SIZE(unit))
 	{
 		shift++;
-		div *= 1000;
+		divisor *= 1000;
 	}
 
-	pr_attr(attr, "%.1f %s", (float)code / div, unit[shift]);
+	pr_attr(attr, "%.1f %s", (float)code / divisor, unit[shift]);
 }
 
 /*
